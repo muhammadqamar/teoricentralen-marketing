db85fd5ffa8a9ae1ca5dbd8f4e0ffaffb723895a
David Boya <david@boya.se>
2023-09-30 18:29:34 +0200

    Cleanup

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -245,13 +245,6 @@ export function Header() {
                 >
                   Om oss
                 </Link>
-                <Link
-                  onClick={() => setMobileMenuOpen(false)}
-                  href="/demo"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  Kontakt
-                </Link>
               </div>
               <div className="py-6">
                 <Button
