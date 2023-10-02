dc5a55b1960c5f321c260dcf9b2971859a2c36b1
muhammadqamar <muhammadqamar111@gmail.com>
2023-10-02 14:25:44 +0500

    add dropdown for footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,11 @@
 // import { ContainerInner, ContainerOuter } from '@/components/Container'
+'use client'
+import { Fragment } from 'react'
 
 import Link from 'next/link'
+import { Menu, Transition } from '@headlessui/react'
+
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
 
 const navigation = {
   educations: [
@@ -19,7 +24,6 @@ const navigation = {
     { name: 'Integritetspolicy', href: '/integritetspolicy' },
   ],
 }
-
 export function Footer() {
   return (
     <footer className="mt-32 flex-none bg-gray-100">
@@ -35,6 +39,81 @@ export function Footer() {
               Plattformen som hjälper trafikskolor att växa. Utvecklat
               tillsammans med Trafikskolor i Sverige.
             </p>
+            <div className="top-16 w-56 text-left">
+              <Menu as="div" className="relative inline-block text-left">
+                <div>
+                  <Menu.Button className="inline-flex w-full justify-center rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+                    Select Language
+                    <ChevronDownIcon
+                      className="-mr-1 ml-2 h-5 w-5 text-violet-200 hover:text-violet-100"
+                      aria-hidden="true"
+                    />
+                  </Menu.Button>
+                </div>
+                <Transition
+                  as={Fragment}
+                  enter="transition ease-out duration-100"
+                  enterFrom="transform opacity-0 scale-95"
+                  enterTo="transform opacity-100 scale-100"
+                  leave="transition ease-in duration-75"
+                  leaveFrom="transform opacity-100 scale-100"
+                  leaveTo="transform opacity-0 scale-95"
+                >
+                  <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <div className="px-1 py-1 ">
+                      <Menu.Item>
+                        {({ active }) => (
+                          <button
+                            className={`${
+                              active
+                                ? 'bg-violet-500 text-white'
+                                : 'text-gray-900'
+                            } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                          >
+                            {/* {active ? (
+                              <EditActiveIcon
+                                className="mr-2 h-5 w-5"
+                                aria-hidden="true"
+                              />
+                            ) : (
+                              <EditInactiveIcon
+                                className="mr-2 h-5 w-5"
+                                aria-hidden="true"
+                              />
+                            )} */}
+                            Sv
+                          </button>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <button
+                            className={`${
+                              active
+                                ? 'bg-violet-500 text-white'
+                                : 'text-gray-900'
+                            } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                          >
+                            {/* {active ? (
+                              <DuplicateActiveIcon
+                                className="mr-2 h-5 w-5"
+                                aria-hidden="true"
+                              />
+                            ) : (
+                              <DuplicateInactiveIcon
+                                className="mr-2 h-5 w-5"
+                                aria-hidden="true"
+                              />
+                            )} */}
+                            en-US
+                          </button>
+                        )}
+                      </Menu.Item>
+                    </div>
+                  </Menu.Items>
+                </Transition>
+              </Menu>
+            </div>
           </div>
           <div className="mt-16 grid grid-cols-2 gap-8 xl:col-span-2 xl:mt-0">
             <div className="md:grid md:grid-cols-2 md:gap-8">
