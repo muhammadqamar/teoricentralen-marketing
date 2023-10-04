11cadd7b4d2c86aba8521e4f1c2e5cbd7f3dd1bc
muhammadqamar <muhammadqamar111@gmail.com>
2023-10-04 20:02:39 +0500

    design blogpage

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -5,6 +5,9 @@ import { FadeIn } from '@/components/FadeIn'
 import { Partners } from '@/components/Partners'
 import { getAllArticles } from '@/lib/articles'
 import { formatDate } from '@/lib/formatDate'
+import Image from 'next/image'
+
+import portraitImage from '@/images/banner.svg'
 
 function Article({ article }) {
   return (
@@ -21,41 +24,48 @@ function Article({ article }) {
   )
 }
 
-export default async function Home({data}) {
+export default async function Home({ data }) {
   let articles = (await getAllArticles()).slice(0, 4)
   console.log(data)
   return (
     <>
-      <Container className="mt-16">
-        <div className="max-w-2xl">
-          <FadeIn className="flex items-center gap-x-8">
-            <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-              {data.heading}
-            </h1>
-          </FadeIn>
-          <FadeIn delay={2.2} className="flex items-center gap-x-8">
-            <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-              Utvecklat tillsammans med trafikskolor i Sverige.
-            </p>
-          </FadeIn>
+      <div className="relative left-0 top-0 h-[650px] w-full overflow-hidden ">
+        <div className="h-full before:absolute before:top-0 before:h-full before:w-full before:bg-[#88b6fc] before:opacity-[0.3]">
+          <Image
+            src={portraitImage}
+            alt=""
+            sizes="(min-width: 1024px) 32rem, 20rem"
+            className="aspect-square h-full w-full bg-zinc-100 object-cover dark:bg-zinc-800"
+          />
         </div>
-      </Container>
+        <Container className="absolute left-0 top-[18%] w-full">
+          <div className="mt-16 max-w-2xl">
+            <FadeIn className="flex items-center gap-x-8">
+              <h1 className="text-4xl font-bold tracking-tight text-white dark:text-zinc-100 sm:text-5xl">
+                {data.heading}
+              </h1>
+            </FadeIn>
+            <FadeIn delay={2.2} className="flex items-center gap-x-8">
+              <p className="mt-6 text-base text-white dark:text-zinc-400">
+                Utvecklat tillsammans med trafikskolor i Sverige.
+              </p>
+            </FadeIn>
+          </div>
 
-      <Container className="mt-16">
-        <div className="flex flex-col max-w-sm gap-2">
-          <Button href="https://app.teoricentralen.se" variant="primary">
-            Logga in som elev
-          </Button>
-          <Button href="https://admin.teoricentralen.se" variant="success">
-            Logga in som trafikskola
-          </Button>
-        </div>
-      </Container>
-
+          <div className="mt-16 flex max-w-sm flex-col gap-2">
+            <Button href="https://app.teoricentralen.se" variant="primary">
+              Logga in som elev
+            </Button>
+            <Button href="https://admin.teoricentralen.se" variant="success">
+              Logga in som trafikskola
+            </Button>
+          </div>
+        </Container>
+      </div>
       <Partners />
 
       <Container className="mt-24 md:mt-28">
-        <div className="grid max-w-xl grid-cols-1 gap-8 mx-auto gap-y-20 lg:max-w-none lg:grid-cols-2">
+        <div className="mx-auto grid max-w-xl grid-cols-1 gap-8 gap-y-20 lg:max-w-none lg:grid-cols-2">
           <div className="flex flex-col gap-16">
             {articles.map((article) => (
               <Article key={article.slug} article={article} />
