acb069c1dcb5cc230f71d870e15f266a8c84eede
David Boya <david@boya.se>
2023-10-06 00:56:28 +0200

    Cleanup

---
+++ src/app/[lang]/korkortsfragor/page.jsx
@@ -0,0 +1,45 @@
+import Image from 'next/image'
+
+import { Container } from '@/components/Container'
+import portraitImage from '@/images/logo.png'
+
+export const metadata = {
+  title: 'Körkortsfrågor',
+  description: 'Framtidens trafikutbildning är här',
+}
+
+export default async function Page({ params }) {
+  return (
+    <Container className="mt-16">
+      <div className="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12">
+        <div className="lg:pl-20">
+          <div className="max-w-xs px-2.5 lg:max-w-none">
+            <Image
+              src={portraitImage}
+              alt=""
+              sizes="(min-width: 1024px) 32rem, 20rem"
+              className="aspect-square rotate-3 object-cover"
+            />
+          </div>
+        </div>
+        <div className="lg:order-first lg:row-span-2">
+          <h1 className="text-4xl font-extrabold tracking-tight text-dark sm:text-5xl">
+            Framtidens trafikutbildning är här.
+          </h1>
+          <div className="mt-6 space-y-7 font-medium text-gray-600">
+            <p>
+              Din verksamhet, dina villkor. Vi hjälper dig att ta din
+              trafikskola till nästa nivå. Se till att du har de verktyg som
+              krävs för att erbjuda dina elever kvalité och samtidigt öka din
+              lönsamhet.
+            </p>
+            <p>
+              Smidig inskrivning av elever. Låt dina kunder själv skriva in sig
+              på din trafikskola med hjälp av enkel BankID identifiering.
+            </p>
+          </div>
+        </div>
+      </div>
+    </Container>
+  )
+}
--- src/app/sitemap.jsx
+++ src/app/sitemap.jsx
@@ -1,19 +1,19 @@
 export default function sitemap() {
   return [
     {
-      url: 'https://beta.teoricentralen.se',
+      url: process.env.NEXT_PUBLIC_SITE_URL,
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 1,
     },
     {
-      url: 'https://beta.teoricentralen.se/about',
+      url: process.env.NEXT_PUBLIC_SITE_URL + '/about',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://beta.teoricentralen.se/demo',
+      url: process.env.NEXT_PUBLIC_SITE_URL + '/demo',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 0.5,
