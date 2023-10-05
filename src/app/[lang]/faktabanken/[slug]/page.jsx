fe85158a37786a6e852a23aed3adffe9fac55683
David Boya <david@boya.se>
2023-10-06 01:50:08 +0200

    Cleanup

--- src/app/[lang]/faktabanken/[slug]/page.jsx
+++ src/app/[lang]/faktabanken/[slug]/page.jsx
@@ -1,23 +1,19 @@
-import FaktabankenSlug from '@/components/Faktabanken/slug'
-import { getAllFacts, getFact } from '@/lib/facts'
-import { draftMode } from 'next/headers'
+// import FaktabankenSlug from '@/components/Faktabanken/slug'
+// import { getAllFacts, getFact } from '@/lib/facts'
+// import { draftMode } from 'next/headers'
 
-export async function generateStaticParams({ params: { lang } }) {
-  const allFacts = await getAllFacts(false, lang)
+// export async function generateStaticParams({ params: { lang } }) {
+//   const allFacts = await getAllFacts(false, lang)
 
-  return allFacts?.map((fact) => ({
-    title: fact.title,
-    slug: fact.slug,
-  }))
-}
+//   return allFacts?.map((fact) => ({
+//     title: fact.title,
+//     slug: fact.slug,
+//   }))
+// }
 
-export default async function Page({ params: { lang, slug } }) {
-  const { isEnabled } = draftMode()
-  const { fact } = await getFact(slug, isEnabled, lang)
+// export default async function Page({ params: { lang, slug } }) {
+//   const { isEnabled } = draftMode()
+//   const { fact } = await getFact(slug, isEnabled, lang)
 
-  // console.log('hello')
-
-  // console.log('params.slug', fact)
-
-  return <FaktabankenSlug data={fact} />
-}
+//   return <FaktabankenSlug data={fact} />
+// }
--- src/middleware.jsx
+++ src/middleware.jsx
@@ -1,31 +1,15 @@
-import { NextResponse } from 'next/server'
-import { match } from '@formatjs/intl-localematcher'
-import Negotiator from 'negotiator'
+import createMiddleware from 'next-intl/middleware'
 
-const locales = ['sv', 'en']
-const defaultLocale = 'sv'
+export default createMiddleware({
+  // A list of all locales that are supported
+  locales: ['sv', 'en'],
 
-export function middleware(request) {
-  // Check if there is any supported locale in the pathname
-  const { pathname } = request.nextUrl
-  const pathnameHasLocale = locales.some(
-    (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`,
-  )
-
-  // If there is a locale, continue
-  if (pathnameHasLocale) return NextResponse.next()
-
-  // If we have no locale, redirect to the preferred locale
-  const locale = defaultLocale
-  request.nextUrl.pathname = `/${locale}${pathname}`
-
-  // e.g. incoming request is /products - the new URL is now /en-US/products
-  return Response.redirect(request.nextUrl)
-}
+  // If this locale is matched, pathnames work without a prefix (e.g. `/about`)
+  defaultLocale: 'sv',
+})
 
 export const config = {
-  // Matcher ignoring `/_next/` and `/api/`
-  matcher: [
-    '/((?!api|_next/static|_next/image|sitemap.xml|manifest.*|favicon.ico).*)',
-  ],
+  // Skip all paths that should not be internationalized. This example skips
+  // certain folders and all pathnames with a dot (e.g. favicon.ico)
+  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)'],
 }
