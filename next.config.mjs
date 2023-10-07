6b9db78da382af5e5c7a327ac9d418917211233a
JakovGlavac <34971091+JakovGlavac@users.noreply.github.com>
2023-10-07 17:03:27 +0200

    sitemap

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,8 @@ const nextConfig = {
   images: {
     domains: ['images.ctfassets.net'],
   },
+  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000',
+  generateRobotsTxt: true, // (optional)
 }
 
 const withMDX = nextMDX({
