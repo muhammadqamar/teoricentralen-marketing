80bcab9617180854057985d27fdd874d79d54635
David Boya <david@boya.se>
2023-10-07 20:50:34 +0200

    Cleanup

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,10 @@ import { usePathname } from 'next-intl/client'
 import { Navigation } from '@/components/Navigation'
 import { Button } from '@/components/Button'
 import bgHome from '@/images/backgrounds/elk.jpg'
+import bgDefault from '@/images/backgrounds/default.jpg'
 import Image from 'next/image'
 import { FadeIn } from './FadeIn'
+import { Container } from './Container'
 
 export function Header({ lang }) {
   const path = usePathname()
@@ -14,60 +16,60 @@ export function Header({ lang }) {
     <div className="bg-dark">
       <Navigation lang={lang} />
 
-      {path === '/' && <Hero />}
-      {path !== '/' && <HeroImage />}
+      {path === '/' && <HomeHero />}
+      {path !== '/' && <PageHero />}
     </div>
   )
 }
 
-function HeroImage() {
+function HeroImage2() {
   return (
-    <>
-      <div className="relative overflow-hidden">
-        <div className="absolute inset-0">
-          <Image
-            src={bgHome}
-            alt="Teoricentralen"
-            className="h-full w-full object-cover object-center"
-          />
-        </div>
-        <div className="relative bg-black bg-opacity-50 px-6 py-32 sm:px-12 sm:py-40 lg:px-16">
-          <div className="relative mx-auto flex max-w-2xl flex-col items-center text-center">
-            <FadeIn className="flex items-center justify-center gap-x-8">
-              <h1 className="text-4xl font-extrabold tracking-tight text-white sm:text-5xl">
-                Framtidens trafikutbildning är här.
-              </h1>
-            </FadeIn>
-          </div>
-        </div>
-      </div>
-    </>
-  )
-}
-
-function Hero() {
-  return (
-    <div className="relative isolate overflow-hidden pt-24">
+    <div className="relative isolate overflow-hidden pt-14">
       <Image
         src={bgHome}
         alt="Teoricentralen"
         className="absolute inset-0 -z-10 h-full w-full object-cover"
       />
 
-      <div
-        className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden bg-black opacity-50 blur-3xl"
-        aria-hidden="true"
-      >
-        <div
-          className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] opacity-20 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-          style={{
-            clipPath:
-              'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-          }}
-        />
+      <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+        <div className="text-center">
+          <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
+            Data to enrich your online business
+          </h1>
+          <p className="mt-6 text-lg leading-8 text-gray-300">
+            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
+            lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
+            fugiat aliqua.
+          </p>
+          <div className="mt-10 flex items-center justify-center gap-x-6">
+            <a
+              href="#"
+              className="rounded-md bg-primary px-3.5 py-2.5 text-sm font-bold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400"
+            >
+              Get started
+            </a>
+            <a href="#" className="text-sm font-semibold leading-6 text-white">
+              Learn more <span aria-hidden="true">→</span>
+            </a>
+          </div>
+        </div>
       </div>
+    </div>
+  )
+}
 
-      <div className="mx-auto max-w-xl pb-40 pt-32">
+function HomeHero() {
+  return (
+    <div className="relative isolate overflow-hidden">
+      <Image
+        src={bgHome}
+        alt="Teoricentralen"
+        className="absolute inset-0 -z-10 h-full w-full object-cover"
+      />
+
+      <div className="absolute inset-0 top-0 -z-10 h-full bg-black bg-opacity-50"></div>
+
+      <Container className="pb-24 pt-40 sm:pb-48 sm:pt-64">
         <div className="text-center">
           <FadeIn className="flex items-center justify-center gap-x-8">
             <h1 className="text-4xl font-extrabold tracking-tight text-white sm:text-5xl">
@@ -92,20 +94,32 @@ function Hero() {
             </div>
           </FadeIn>
         </div>
-      </div>
+      </Container>
+    </div>
+  )
+}
 
-      <div
-        className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-        aria-hidden="true"
-      >
-        <div
-          className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-          style={{
-            clipPath:
-              'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-          }}
-        />
-      </div>
+function PageHero() {
+  return (
+    <div className="relative isolate overflow-hidden">
+      <Image
+        src={bgDefault}
+        alt="Teoricentralen"
+        className="absolute inset-0 -z-10 h-full w-full object-cover"
+      />
+
+      <div className="absolute inset-0 top-0 -z-10 h-full bg-black bg-opacity-50"></div>
+
+      <Container className="pb-24 pt-48">
+        <h2 className="text-4xl font-extrabold tracking-tight text-white sm:text-6xl">
+          Support center
+        </h2>
+        <p className="mt-6 text-lg leading-8 text-gray-200">
+          Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem
+          cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat
+          aliqua.
+        </p>
+      </Container>
     </div>
   )
 }
