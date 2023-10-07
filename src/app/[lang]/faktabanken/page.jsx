505853253bd7c0e8f72188cc4da8a30f3ae7f7ee
David Boya <david@boya.se>
2023-10-07 19:34:13 +0200

    Cleanup

--- src/app/[lang]/faktabanken/page.jsx
+++ src/app/[lang]/faktabanken/page.jsx
@@ -2,11 +2,9 @@ import { getAllFacts } from '@/lib/facts'
 import { draftMode } from 'next/headers'
 import Faktabanken from '@/components/Faktabanken'
 
-export default async function Page({ params }) {
+export default async function Page({ params: { lang } }) {
   const { isEnabled } = draftMode()
-  const allFacts = await getAllFacts(isEnabled, params.lang)
-
-  // console.log('allFacts', allFacts)
+  const allFacts = await getAllFacts(isEnabled, lang)
 
   return <Faktabanken data={allFacts} />
 }
