0644e2f0a48c0bd84781a668f564d132e6f89ecd
David Boya <david@boya.se>
2023-10-08 18:24:47 +0200

    Cleanup

--- src/app/[locale]/utbildningar/motorcykel/page.jsx
+++ src/app/[locale]/utbildningar/motorcykel/page.jsx
@@ -19,7 +19,7 @@ export default async function Page({ params: { locale } }) {
   return (
     <>
       <PageHero title={title} description={description} image={image} />
-      <Container className="my-16">MotorCycle</Container>
+      <Container className="my-16">Körkortsteori för motorcykel</Container>
     </>
   )
 }
