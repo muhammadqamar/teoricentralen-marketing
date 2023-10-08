a5278528c8c87879cbd41a630711ad4928335b7c
JakovGlavac <34971091+JakovGlavac@users.noreply.github.com>
2023-10-08 16:18:11 +0200

    Fixed navigation problem on click

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -146,9 +146,8 @@ export function Navigation({ locale }) {
         <div className="flex lg:hidden">
           <button
             type="button"
-            className={`-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-white ${
-              mobileMenuOpen ? 'hidden' : ''
-            }`}
+            className={`-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-white ${mobileMenuOpen ? 'hidden' : ''
+              }`}
             onClick={() => setMobileMenuOpen(true)}
           >
             <span className="sr-only">Open main menu</span>
@@ -160,15 +159,13 @@ export function Navigation({ locale }) {
           <Popover>
             <Popover.Button
               ref={ref}
-              className={`flex items-center gap-x-1 text-sm font-bold leading-6 ${
-                open ? 'text-dark' : 'text-white'
-              }`}
+              className={`flex items-center gap-x-1 text-sm font-bold leading-6 ${open ? 'text-dark' : 'text-white'
+                }`}
             >
               Utbildningar
               <ChevronDownIcon
-                className={`h-5 w-5 flex-none ${
-                  open ? 'text-dark' : 'text-white'
-                }`}
+                className={`h-5 w-5 flex-none ${open ? 'text-dark' : 'text-white'
+                  }`}
                 aria-hidden="true"
               />
             </Popover.Button>
@@ -186,57 +183,64 @@ export function Navigation({ locale }) {
                 // ref={setReferenceElement}
                 className="absolute inset-x-0 top-0 -z-10 bg-white pt-14 shadow-lg ring-1 ring-gray-900/5"
               >
-                <div className="mx-auto grid max-w-7xl grid-cols-3 gap-x-4 px-6 py-10 lg:px-8 xl:gap-x-4">
-                  {educations.map((item) => (
-                    <div
-                      key={item.name}
-                      className="group relative rounded-lg p-6 text-sm leading-6 hover:bg-gray-50"
-                    >
-                      <div className="flex h-12 w-12 items-center justify-center">
-                        <Image
-                          src={item.icon}
-                          alt={item.name}
-                          className="h-8"
-                          unoptimized
-                        />
-                      </div>
-
-                      <Link
-                        href={item.href}
-                        className="mt-4 block font-bold text-dark"
-                        locale={locale}
-                      >
-                        {item.name}
-                        <span className="absolute inset-0" />
-                      </Link>
-
-                      <p className="font-semibold text-gray-600">
-                        {item.description}
-                      </p>
-                    </div>
-                  ))}
-                </div>
-
-                <div className="bg-gray-50">
-                  <div className="mx-auto max-w-7xl px-6 lg:px-8">
-                    <div className="grid grid-cols-3 divide-x divide-gray-900/5 border-x border-gray-900/5">
-                      {callsToAction.map((item) => (
-                        <Link
+                {({ close }) => (
+                  <>
+                    <div className="mx-auto grid max-w-7xl grid-cols-3 gap-x-4 px-6 py-10 lg:px-8 xl:gap-x-4">
+                      {educations.map((item) => (
+                        <div
                           key={item.name}
-                          href={item.href}
-                          className="flex items-center justify-center gap-x-2.5 p-3 text-sm font-semibold leading-6 text-dark hover:bg-gray-100"
-                          locale={locale}
+                          className="group relative rounded-lg p-6 text-sm leading-6 hover:bg-gray-50"
+
                         >
-                          <item.icon
-                            className="h-5 w-5 flex-none text-gray-500"
-                            aria-hidden="true"
-                          />
-                          {item.name}
-                        </Link>
+                          <div className="flex h-12 w-12 items-center justify-center">
+                            <Image
+                              src={item.icon}
+                              alt={item.name}
+                              className="h-8"
+                              unoptimized
+                            />
+                          </div>
+
+                          <Link
+                            href={item.href}
+                            className="mt-4 block font-bold text-dark"
+                            locale={locale}
+                            onClick={() => close()}
+                          >
+                            {item.name}
+                            <span className="absolute inset-0" />
+                          </Link>
+
+                          <p className="font-semibold text-gray-600">
+                            {item.description}
+                          </p>
+                        </div>
                       ))}
                     </div>
-                  </div>
-                </div>
+
+
+                    <div className="bg-gray-50">
+                      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                        <div className="grid grid-cols-3 divide-x divide-gray-900/5 border-x border-gray-900/5">
+                          {callsToAction.map((item) => (
+                            <Link
+                              key={item.name}
+                              href={item.href}
+                              className="flex items-center justify-center gap-x-2.5 p-3 text-sm font-semibold leading-6 text-dark hover:bg-gray-100"
+                              locale={locale}
+                            >
+                              <item.icon
+                                className="h-5 w-5 flex-none text-gray-500"
+                                aria-hidden="true"
+                              />
+                              {item.name}
+                            </Link>
+                          ))}
+                        </div>
+                      </div>
+                    </div>
+                  </>
+                )}
               </Popover.Panel>
             </Transition>
           </Popover>
