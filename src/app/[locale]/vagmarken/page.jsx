5e77697fa0c537fbff91bdf30990a69ebc25f04e
muhammadqamar <muhammadqamar111@gmail.com>
2023-10-09 12:44:29 +0500

    fetch road signs

--- src/app/[locale]/vagmarken/page.jsx
+++ src/app/[locale]/vagmarken/page.jsx
@@ -3,6 +3,10 @@ import { PageHero } from '@/components/Hero/PageHero'
 import { Container } from '@/components/Container'
 import portraitImage from '@/images/logo.png'
 import backgroundImage from '@/images/backgrounds/vagmarke.jpg'
+import { draftMode } from 'next/headers'
+import { getAllRoadSign } from '@/lib/roadSign'
+import Link from 'next/link'
+import PlaceholderImg from '@/images/og-image.png'
 
 const title = 'Vägmärken'
 const description = 'Alla Sveriges Vägmärken'
@@ -18,7 +22,12 @@ export const metadata = {
   },
 }
 
-export default async function Page({ params }) {
+export default async function Page({ params: { locale } }) {
+  const { isEnabled } = draftMode()
+  const allRoadSign = await getAllRoadSign(isEnabled, locale)
+
+  console.log('allRoadSign', allRoadSign)
+
   return (
     <>
       <PageHero
@@ -27,7 +36,35 @@ export default async function Page({ params }) {
         description={description}
       />
 
-      <Container className="my-16">Alla Sveriges vägmärken</Container>
+      <Container className="my-16">
+        <div className=" grid max-w-[1120px] grid-cols-4  gap-6 space-y-16">
+          {allRoadSign?.map((sign, index) => (
+            <Link
+              className="!m-0 h-full"
+              href={`/faktabanken/${sign?.slug}`}
+              key={sign.slug}
+            >
+              <div className="h-full min-h-[200px] w-full overflow-hidden rounded-md bg-white shadow-md">
+                <div className="relative h-[100px] w-full">
+                  <Image
+                    src={sign?.image?.url || PlaceholderImg}
+                    loading="lazy"
+                    alt=""
+                    fill
+                    className="inset-0 aspect-square h-full w-full animate-pulse  "
+                    placeholder={
+                      'data:image/jpeg;base64,/9j/4gxYSUNDX1BST0ZJTEUAAQEAAAxITGlub...'
+                    }
+                  />
+                </div>
+                <div className="bg-white px-5 py-4 ">
+                  <p className="mb-2">{sign?.title}</p>
+                </div>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </Container>
     </>
   )
 }
---
+++ src/lib/roadSign.js
@@ -0,0 +1,87 @@
+const POST_GRAPHQL_FIELDS = `
+  slug
+  title
+  image {
+    url
+  }
+
+`
+
+async function fetchGraphQL(query, preview = false) {
+  return fetch(
+    `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${
+          preview
+            ? process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN
+            : process.env.CONTENTFUL_ACCESS_TOKEN
+        }`,
+      },
+      body: JSON.stringify({ query }),
+      next: { tags: ['posts'] },
+      cache: 'no-store',
+    },
+  ).then((response) => response.json())
+}
+
+function extractFact(fetchResponse) {
+  return fetchResponse?.data?.vagmarkeskategoriCollection?.items?.[0]
+}
+
+function extractFactEntries(fetchResponse) {
+  return fetchResponse?.data?.vagmarkeskategoriCollection?.items
+}
+
+export async function getPreviewPostBySlug(slug) {
+  const entry = await fetchGraphQL(
+    `query {
+    vagmarkeskategoriCollection( where: { slug: "${slug}" }, preview: true, limit: 1) {
+      items {
+        ${POST_GRAPHQL_FIELDS}
+      }
+    }
+  }`,
+    true,
+  )
+
+  return extractPost(entry)
+}
+
+export async function getAllRoadSign(isDraftMode, locale) {
+  const entries = await fetchGraphQL(
+    `query {
+      vagmarkeskategoriCollection(locale: "${locale}" , where: { slug_exists: true }, preview: ${
+        isDraftMode ? 'true' : 'false'
+      }) {
+        items {
+          ${POST_GRAPHQL_FIELDS}
+        }
+      }
+    }`,
+    isDraftMode,
+  )
+
+  return extractFactEntries(entries)
+}
+
+export async function getRoadSign(slug, preview, locale) {
+  const entry = await fetchGraphQL(
+    `query {
+      vagmarkeskategoriCollection(locale: "${locale}", where: { slug: "${slug}" }, preview: ${
+        preview ? 'true' : 'false'
+      }, limit: 1) {
+        items {
+          ${POST_GRAPHQL_FIELDS}
+        }
+      }
+    }`,
+    preview,
+  )
+
+  return {
+    fact: extractFact(entry),
+  }
+}
