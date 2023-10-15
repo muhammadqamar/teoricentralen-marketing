fcd204b279594d9e90d807dff79b21a766925ae6
David Boya <david@boya.se>
2023-10-15 23:13:18 +0200

    Cleanup

--- src/utils/readMore.jsx
+++ src/utils/readMore.jsx
@@ -1,20 +1,28 @@
 'use client'
-import React, { useState } from "react";
+import React, { useState } from 'react'
 export function ReadMore({ content, moreContent }) {
+  const [isExpanded, setIsExpanded] = useState(false)
 
-    const [isExpanded, setIsExpanded] = useState(false);
+  // Function to toggle the read more/less state
+  const toggleReadMore = () => {
+    setIsExpanded(!isExpanded)
+  }
 
-    // Function to toggle the read more/less state
-    const toggleReadMore = () => {
-        setIsExpanded(!isExpanded);
-    };
-
-    return (
-        <div className="my-[20px]">
-            <p>{content}</p>
-            <p className="overflow-hidden " style={{ height: isExpanded ? 'auto' : '0' }}>{moreContent}</p>
-            <button className="flex m-auto italic underline" onClick={() => toggleReadMore()}>{isExpanded ? 'Read Less' : 'Read More'}</button>
-
-        </div>
-    );
+  return (
+    <div className="my-[20px]">
+      <p>{content}</p>
+      <p
+        className="overflow-hidden "
+        style={{ height: isExpanded ? 'auto' : '0' }}
+      >
+        {moreContent}
+      </p>
+      <button
+        className="m-auto flex italic underline"
+        onClick={() => toggleReadMore()}
+      >
+        {isExpanded ? 'Read Less' : 'Read More'}
+      </button>
+    </div>
+  )
 }
