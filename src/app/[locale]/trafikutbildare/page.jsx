7a8ea4d0f16eb61997eb84e1b689ca6a7987be49
kimpoliini <kimpas@hotmail.se>
2023-10-17 01:13:56 +0200

    Added first section from teoricentralen.se/funktioner

--- src/app/[locale]/trafikutbildare/page.jsx
+++ src/app/[locale]/trafikutbildare/page.jsx
@@ -1,6 +1,8 @@
 import { Container } from '@/components/Container'
 import { PageHero } from '@/components/Hero/PageHero'
 import backgroundImage from '@/images/backgrounds/trafikutbildare.jpg'
+import denny from '@/images/testimonials/denny.jpg'
+import Image from 'next/image'
 
 const title = 'För trafikutbildare'
 const description = 'Plattformen som hjälper trafikskolor att växa'
@@ -25,7 +27,10 @@ export default async function Page({ params }) {
         backgroundImage={backgroundImage}
       />
 
-      <Container className="my-16">
+      <Container>
+        <FeatureSection />
+
+        {/* placeholder content */}
         <div className="mx-auto max-w-7xl px-6 lg:flex lg:px-8">
           <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-12 gap-y-16 lg:mx-0 lg:min-w-full lg:max-w-none lg:flex-none lg:gap-y-8">
             <div className="lg:col-end-1 lg:w-full lg:max-w-lg lg:pb-8">
@@ -87,5 +92,43 @@ export default async function Page({ params }) {
         </div>
       </Container>
     </>
+  )
+}
+
+export function FeatureSection() {
+  return (
+    <div className="overflow-hidden bg-white py-24 sm:py-16">
+      <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-center">
+        <div>
+          <div className="lg:max-w-lgy">
+            <h1 className="text-4xl font-bold tracking-tight sm:text-6xl">Vi stöttar din verksamhet</h1>
+            <blockquote className="text-lg leading-7 mt-8">
+              <p>
+                “Vi utvecklar ditt nya affärssystem för att möta dina framtida utmaningar. Hos oss bedriver du din verksamhet på dina villkor.”
+              </p>
+            </blockquote>
+            <figcaption className="mt-6 flex gap-x-4 text-base leading-6">
+              <Image
+                width={64}
+                height={64}
+                src={denny}
+                alt="Denny Soulaka"
+                className="h-12 w-12 flex-none rounded-full"
+              />
+              <div>
+                <p className="text-gray-900">Denny Soulaka</p>
+                <p className="font-semibold">Teoricentralen Sverige AB </p>
+              </div>
+            </figcaption>
+          </div>
+        </div>
+        <video
+          className='rounded'
+          autoPlay
+          loop
+          src='https://uploads-ssl.webflow.com/63eaaf48b5d45c04b3775fc9/640a4874cdb3d1770f94a5fe_enkel%20view-transcode.mp4'
+        />
+      </div>
+    </div>
   )
 }
