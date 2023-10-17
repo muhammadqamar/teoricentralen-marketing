4207fb2b3c310145eb1a6f40272d78375a18447c
kimpoliini <kimpas@hotmail.se>
2023-10-18 00:34:57 +0200

    Updated FeatureSection to support forced vertical sections

--- src/components/FeatureSection.jsx
+++ src/components/FeatureSection.jsx
@@ -1,13 +1,25 @@
 import Image from "next/image";
 
 
-export default function FeatureSection({ title, text, image, isVideo = false, isFlipped = false, children }) {
+export default function FeatureSection(props) {
+    const {
+        title,
+        text,
+        image,
+        isVideo,
+        isFlipped,
+        isVertical,
+        children
+    } = props
+
     return (
-        <div className="overflow-hidden bg-white py-16 sm:py-16 my-8 rounded-[2rem]">
-            <div className={`mx-8 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:max-w-none lg:grid-cols-2 lg:items-center`}>
+        <div className="overflow-hidden bg-white md:p-8 lg:py-16 rounded-[2rem] lg:m-8 m-4">
+            <div className={`lg:mx-8 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:max-w-none 
+            ${isVertical ? "lg:grid-cols-1" : "lg:grid-cols-2  lg:items-center"}
+            `}>
                 <div>
                     <div className="lg:max-w-lgy">
-                        <h1 className="text-4xl font-extrabold tracking-tight sm:text-6xl whitespace-break-spaces">{title}</h1>
+                        <h1 className="xl:text-5xl font-extrabold tracking-tight sm:text-4xl">{title}</h1>
                         <blockquote className="text-lg leading-7 mt-8">
                             <p>{text}</p>
                         </blockquote>
@@ -24,9 +36,9 @@ export default function FeatureSection({ title, text, image, isVideo = false, is
                         />
                         :
                         <Image
+                            className="rounded-lg"
                             width={"500"}
                             height={"500"}
-                            className="rounded-lg"
                             src={image} />}
                 </div>
             </div>
