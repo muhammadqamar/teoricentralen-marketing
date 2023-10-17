32326f272419fc58cdb957c33940ae2144837b7e
kimpoliini <kimpas@hotmail.se>
2023-10-17 23:35:33 +0200

    Updated FeatureSection CSS to support left-hand images

--- src/components/FeatureSection.jsx
+++ src/components/FeatureSection.jsx
@@ -1,32 +1,34 @@
 import Image from "next/image";
 
 
-export default function FeatureSection({ title, text, image, isVideo = false, children }) {
+export default function FeatureSection({ title, text, image, isVideo = false, isFlipped = false, children }) {
     return (
-        <div className="overflow-hidden bg-white py-24 sm:py-16">
-            <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-center">
+        <div className="overflow-hidden bg-white py-16 sm:py-16 my-8 rounded-[2rem]">
+            <div className={`mx-8 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:max-w-none lg:grid-cols-2 lg:items-center`}>
                 <div>
                     <div className="lg:max-w-lgy">
-                        <h1 className="text-4xl font-bold tracking-tight sm:text-6xl">{title}</h1>
+                        <h1 className="text-4xl font-extrabold tracking-tight sm:text-6xl whitespace-break-spaces">{title}</h1>
                         <blockquote className="text-lg leading-7 mt-8">
                             <p>{text}</p>
                         </blockquote>
                         {children}
                     </div>
                 </div>
-                {isVideo ?
-                    <video
-                        className='rounded'
-                        autoPlay
-                        loop
-                        src={image}
-                    />
-                    :
-                    <Image
-                        width={"500"}
-                        height={"500"}
-                        className="rounded"
-                        src={image} />}
+                <div className={`${isFlipped ? "lg:-order-1" : ""}`}>
+                    {isVideo ?
+                        <video
+                            className="rounded-lg"
+                            autoPlay
+                            loop
+                            src={image}
+                        />
+                        :
+                        <Image
+                            width={"500"}
+                            height={"500"}
+                            className="rounded-lg"
+                            src={image} />}
+                </div>
             </div>
         </div>
     )
