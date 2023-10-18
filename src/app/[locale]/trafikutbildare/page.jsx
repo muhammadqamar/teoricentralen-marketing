79c944dba285640eef86cfe42b254795a211020d
kimpoliini <kimpas@hotmail.se>
2023-10-18 16:38:38 +0200

    Commented out placeholder content, fixed FAQ margin in /trafikutbildare

--- src/app/[locale]/trafikutbildare/page.jsx
+++ src/app/[locale]/trafikutbildare/page.jsx
@@ -134,38 +134,40 @@ export default async function Page({ params }) {
           </div>
         </FeatureSection>
 
-       <div className='m-16'>
-            <Faq />
-       </div>
+        <div className='mt-16'>
+          <Faq />
+        </div>
 
 
         {/* placeholder content */}
-        <div className="mx-auto max-w-7xl px-6 lg:flex lg:px-8">
+        {/* <div className="mx-auto max-w-7xl px-6 lg:flex lg:px-8">
           <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-12 gap-y-16 lg:mx-0 lg:min-w-full lg:max-w-none lg:flex-none lg:gap-y-8">
-            <div className="lg:col-end-1 lg:w-full lg:max-w-lg lg:pb-8">
-              <h2 className="text-3xl font-extrabold tracking-tight text-dark">
-                {title}
-              </h2>
+            <div className="lg:col-end-1 lg:w-full lg:max-w-lg lg:pb-8"> */}
 
-              <p className="mt-6 text-xl leading-8 text-gray-600">
-                {description}
-              </p>
+        {/* <h2 className="text-3xl font-extrabold tracking-tight text-dark">
+          {title}
+        </h2>
 
-              <p className="mt-6 text-base leading-7 text-gray-600">
-                Vill du ta din verksamhet till nästa nivå? Upptäck hur vi kan
-                hjälpa dig att automatisera och digitalisera din verksamhet.
-              </p>
+        <p className="mt-6 text-xl leading-8 text-gray-600">
+          {description}
+        </p>
 
-              <div className="flex mt-6">
-                <a
-                  href="/demo"
-                  className="rounded-full bg-primary px-6 py-2.5 text-sm font-bold text-white shadow-sm hover:bg-primary-dark focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Boka en demo <span aria-hidden="true">&rarr;</span>
-                </a>
-              </div>
-            </div>
-            <div className="flex flex-wrap items-start justify-end gap-6 sm:gap-8 lg:contents">
+        <p className="mt-6 text-base leading-7 text-gray-600">
+          Vill du ta din verksamhet till nästa nivå? Upptäck hur vi kan
+          hjälpa dig att automatisera och digitalisera din verksamhet.
+        </p>
+
+        <div className="flex mt-6">
+          <a
+            href="/demo"
+            className="rounded-full bg-primary px-6 py-2.5 text-sm font-bold text-white shadow-sm hover:bg-primary-dark focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Boka en demo <span aria-hidden="true">&rarr;</span>
+          </a>
+        </div> */}
+
+        {/* </div> */}
+        {/* <div className="flex flex-wrap items-start justify-end gap-6 sm:gap-8 lg:contents">
               <div className="flex-auto w-0 lg:ml-auto lg:w-auto lg:flex-none lg:self-end">
                 <div className="aspect-[7/5] overflow-hidden w-[37rem] relative max-w-none rounded-2xl bg-gray-50 object-cover"
                 >
@@ -223,7 +225,7 @@ export default async function Page({ params }) {
               </div>
             </div>
           </div>
-        </div>
+        </div> */}
       </Container>
     </>
   )
