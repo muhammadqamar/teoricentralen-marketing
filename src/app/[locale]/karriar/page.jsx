cfdd95f771717e560c97ce55de5a083af5415bc4
muhammadqamar <muhammadqamar111@gmail.com>
2023-10-19 18:24:05 +0500

    fix build issue

--- src/app/[locale]/karriar/page.jsx
+++ src/app/[locale]/karriar/page.jsx
@@ -21,7 +21,7 @@ export async function generateMetadata({ params: { locale } }) {
   }
 }
 
-export default async function Page({ params }) {
+export default async function Page({ params: { locale } }) {
   return (
     <>
       <PageHero
